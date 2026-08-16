079eb48276ae35fbbe20a42ce3822895d89dd0f2
Hoàng Quang Anh <qanhoang061107@gmail.com>
2026-08-16 19:51:09 +0700

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,34 @@
+
+
+class Solution {
+    public boolean isValid(String s) {
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (c == ')' && top != '(') {
+                    return false;
+                }
+
+                if (c == ']' && top != '[') {
+                    return false;
+                }
+
+                if (c == '}' && top != '{') {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
